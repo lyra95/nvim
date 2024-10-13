7be3b6585950adc812ec0370319ac4e8b19554cd
jo <95hyouka@gmail.com>
2024-10-13 10:13:21 +0000

    this option enables `goto definition`

--- modules/lsp/nix.nix
+++ modules/lsp/nix.nix
@@ -6,6 +6,7 @@
       servers = {
         nil-ls.enable = true;
         nil-ls.settings.formatting.command = ["alejandra"];
+        nil-ls.settings.nix.flake.autoArchive = true; # setting true, this would use networks to fetch codes
         nixd.enable = true;
       };
     };
