03181df696b5c858a5ab42e557fb7f422fc4abed
jo <95hyouka@gmail.com>
2024-10-13 15:57:57 +0000

    disable nixd lsp

--- modules/lsp/nix.nix
+++ modules/lsp/nix.nix
@@ -7,7 +7,6 @@
         nil-ls.enable = true;
         nil-ls.settings.formatting.command = ["alejandra"];
         nil-ls.settings.nix.flake.autoArchive = true; # setting true, this would use networks to fetch codes
-        nixd.enable = true;
       };
     };
   };
