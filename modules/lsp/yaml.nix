cd2316b9f1aae30d001b90903027059bff10648c
jo <95hyouka@gmail.com>
2024-10-13 10:12:53 +0000

    update k8s yaml schema

--- modules/lsp/yaml.nix
+++ modules/lsp/yaml.nix
@@ -27,7 +27,7 @@
                 "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
                 "http://json.schemastore.org/prettierrc" = ".prettierrc.{yml,yaml}";
                 "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-                "https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json" = "*.{yml,yaml}";
+                "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.24.9-standalone-strict/all.json" = "*.{yml,yaml}";
               };
             };
           };
