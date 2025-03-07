22db89abe2c1a143a46d813d02e62e8940627ea7
jo <95hyouka@gmail.com>
2025-03-07 10:39:53 +0000

    delete dangling inputs override

--- flake.nix
+++ flake.nix
@@ -3,12 +3,19 @@
 
   inputs = {
     systems.url = "github:nix-systems/default";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.systems.follows = "systems";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
     nixpkgs.url = "github:NixOS/nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.devshell.follows = "devshell";
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
